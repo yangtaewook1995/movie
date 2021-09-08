31c5d35e1149373f470c92c4708ca43670960b50
Wook <wooke1995@gmail.com>
2021-09-08 12:14:15 +0900

    add state function

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,73 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({ fav, picture, rating }) {
-  return (
-    <div>
-      <h2>I Love {fav}</h2>
-      <h4>{rating}/5</h4>
-      <img src={picture} alt={fav}></img>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 4,
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 3,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 5,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 4.5,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 3.5,
-  },
-];
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
+  }
 
-function renderFood(dish) {
-  return (
-    <Food
-      key={dish.id}
-      fav={dish.name}
-      picture={dish.image}
-      rating={dish.rating}
-    ></Food>
-  );
-}
+  componentDidUpdate() {}
 
-Food.propTypes = {
-  fav: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
+  render() {
+    const { isLoading } = this.state;
 
-function App() {
-  return <div className="App">{foodILike.map(renderFood)}</div>;
+    return <div>{isLoading ? "Loading..." : "We are Ready"}</div>;
+  }
 }
 
 export default App;
